0783e149bc5c3fd8b1baec05ca4e8675a856f74d
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-03-23 15:46:09 +0430

    d

--- cypress/integration/basics_spec.js
+++ cypress/integration/basics_spec.js
@@ -1,8 +1,19 @@
 describe("Basics", () => {
+  it("successfully loads", () => {
+    cy.visit('/');
+  });
   it('requesting / redirects to /posts', () => {
     cy.visit("/");
     cy.url().should("include", "/posts");
   });
+  it('successfully loads post pages', () => {
+    cy.visit("/");
+    cy.get("[role=post-link]").first().click().then((link) => {
+      const url = Cypress.$(link).attr('href')
+      cy.url({ timeout: 30000 }).should('contain', url)
+      cy.get('[role=tags-section]').scrollIntoView({ duration: 30000 })
+    })
+  })
 });
 
 
